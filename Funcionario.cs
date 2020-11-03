b72feed89b6bcd89e25cbbefdc74e9034a3b1292
Venzke <67403605+Venzke@users.noreply.github.com>
2020-11-03 14:26:05 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2_ClassMethod_C_Shap
+{
+    class Funcionario
+    {
+        // Atributos
+        public string Nome;
+        public double Salario;
+        public int Imposto;
+        public float Aumento;
+
+        public double Calc_Imposto() //Método para o calculo do imposto.
+        {
+            double resultado = Salario - Imposto; 
+
+            Salario = resultado; // Salario recebe a atualização, para que os outros métodos possam calcular com o salario atual e não o inserido.
+
+            return Salario; 
+        }
+    
+        public double Calc_Aumento() //Método para o calculo do Aumento.
+        {
+            double resultado = Aumento / 100; // Calculo de porcetagem.
+
+            resultado++; // Recebe +1 para depois multiplicar pelo salário atualizado.
+
+            return resultado * Salario;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2_ClassMethod_C_Shap
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Funcionario p = new Funcionario();
+
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Salário: ");
+            p.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Imposto: ");
+            p.Imposto = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Aumento: ");
+            p.Aumento = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Funcionario {0}, {1} Salário, {2} Imposto.", p.Nome, p.Salario, p.Imposto);
+            Console.WriteLine("\n");
+
+            double Sal = p.Calc_Imposto();
+
+            Console.Write("Devido ao imposto seu salário é de {0}.", Sal);
+            Console.WriteLine("\n");
+
+            double Aumento = p.Calc_Aumento();
+
+            Console.Write("Mas {0} obteve um aumento de {1}%. Seu salário agora é de {2}", p.Nome, p.Aumento, Aumento);
+
+            Console.Read();
+        }
+    }
+}
